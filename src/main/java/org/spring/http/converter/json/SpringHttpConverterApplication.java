b3a4ae6d48644950cba89322c20a0c380a8b85f1
이용규 <yongkyu.lee@navercorp.com>
2020-06-26 18:13:22 +0900

    Initial commit

---
+++ src/main/java/org/spring/http/converter/json/SpringHttpConverterApplication.java
@@ -0,0 +1,13 @@
+package org.spring.http.converter.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHttpConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHttpConverterApplication.class, args);
+	}
+
+}
